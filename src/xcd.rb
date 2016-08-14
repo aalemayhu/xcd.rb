5e1c1b4247fe1d0752167b5f3af21567de37a972
Alexander Alemayhu <alexander@alemayhu.com>
2016-08-14 12:25:26 +0200

    Reformat code
    o Drop unnecessary return.

--- src/xcd.rb
+++ src/xcd.rb
@@ -1,38 +1,38 @@
 #!/usr/bin/ruby
 
 def paths
-    home = ENV['HOME']
-    src = "#{home}/src"
-    github = "#{src}/github.com"
-    return {
-        "me" => "#{github}/scanf",
-        "w" => "#{github}/profsys",
-        "d" => "#{home}/Dropbox/Downloads",
-        "a" => "#{github}/apple",
-        "c" => "#{home}/Library/Mobile Documents/com~apple~CloudDocs",
-        "t" =>  "/tmp/",
-        "k" => "#{src}/kernel.org"
+  home = ENV['HOME']
+  src = "#{home}/src"
+  github = "#{src}/github.com"
+  {
+      "me" => "#{github}/scanf",
+      "w" => "#{github}/profsys",
+      "d" => "#{home}/Dropbox/Downloads",
+      "a" => "#{github}/apple",
+      "c" => "#{home}/Library/Mobile Documents/com~apple~CloudDocs",
+      "t" => "/tmp/",
+      "k" => "#{src}/kernel.org"
     }
-  end
+end
 
-  def navigateTo(dir)
-    puts "GOT: #{dir}"
-    `mkdir -p '#{paths[dir]}'`
-    Dir.chdir("#{paths[dir]}")
-    system("bash")
-  end
+def navigateTo(dir)
+  puts "GOT: #{dir}"
+  `mkdir -p '#{paths[dir]}'`
+  Dir.chdir("#{paths[dir]}")
+  system("bash")
+end
 
-  def usage
-    puts "USAGE: xcd d"
-    puts"\nSupported options:"
-    paths.each_pair do |ykey, value|
-      puts "#{ykey} - #{value}"
-    end
+def usage
+  puts "USAGE: xcd d"
+  puts "\nSupported options:"
+  paths.each_pair do |ykey, value|
+    puts "#{ykey} - #{value}"
   end
+end
 
-  a = ARGV[0] || "me"
-  if a.start_with?("help")
-    usage()
-  else
-    navigateTo(a)
-  end
+a = ARGV[0] || "me"
+if a.start_with?("help")
+  usage()
+else
+  navigateTo(a)
+end
